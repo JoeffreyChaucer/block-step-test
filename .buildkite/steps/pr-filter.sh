4bd112a3674b378e5180dc215966a13670f5ba04
Joeffrey <joeffrey.chaucer.paitan@gmail.com>
2022-06-03 00:48:44 +0800

    fix cannot unmashed

--- .buildkite/steps/pr-filter.sh
+++ .buildkite/steps/pr-filter.sh
@@ -2,11 +2,12 @@
 set -euo pipefail
 
 echo "$BUILDKITE_COMMIT"
+
 echo "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"
 
-git diff --stat "$BUILDKITE_PULL_REQUEST_BASE_BRANCH".."$BUILDKITE_COMMIT"
+git diff --stat $BUILDKITE_PULL_REQUEST_BASE_BRANCH..$BUILDKITE_COMMIT
 
-if ! git diff --name-only "$BUILDKITE_PULL_REQUEST_BASE_BRANCH".."$BUILDKITE_COMMIT" | grep -qvE '(.md)'
+if ! git diff --name-only $BUILDKITE_PULL_REQUEST_BASE_BRANCH..$BUILDKITE_COMMIT | grep -qvE '(.md)'
 then
     echo "Only doc files were updated, not running the CI."
     exit
