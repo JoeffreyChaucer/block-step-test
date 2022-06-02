6816fe5aaed7d808ae6eaab22889f8019b9d618a
Joeffrey <joeffrey.chaucer.paitan@gmail.com>
2022-06-03 00:38:50 +0800

    add steps:

--- .buildkite/steps/pr-filter.sh
+++ .buildkite/steps/pr-filter.sh
@@ -18,6 +18,8 @@ echo "Running pr-validation pipeline."
 echo "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"
 echo "$BUILDKITE_PULL_REQUEST_DRAFT"
 
+echo "steps:"
+
 if [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "develop" ] || [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ] || [[ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" =~ /^release\// ]] || [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "marvel/develop" ] || [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "test_ci" ] &&  ! BUILDKITE_PULL_REQUEST_DRAFT;
 then
     echo " - command: buildkite-agent pipeline upload .buildkite/pipeline.yml"
