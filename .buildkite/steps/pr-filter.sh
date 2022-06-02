e314fd8c3b37de31bcd2a670b58631e0b7ede4c9
Joeffrey <joeffrey.chaucer.paitan@gmail.com>
2022-06-03 00:50:19 +0800

    fix cannot unmashed

--- .buildkite/steps/pr-filter.sh
+++ .buildkite/steps/pr-filter.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-echo $BUILDKITE_COMMIT
 
-echo $BUILDKITE_PULL_REQUEST_BASE_BRANCH
+echo "steps:"
 
-git diff --stat $BUILDKITE_PULL_REQUEST_BASE_BRANCH..$BUILDKITE_COMMIT
 
-if ! git diff --name-only $BUILDKITE_PULL_REQUEST_BASE_BRANCH..$BUILDKITE_COMMIT | grep -qvE '(.md)'
+echo "$BUILDKITE_COMMIT"
+
+echo "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"
+
+
+git diff --stat "$BUILDKITE_PULL_REQUEST_BASE_BRANCH".."$BUILDKITE_COMMIT"
+
+if ! git diff --name-only "$BUILDKITE_PULL_REQUEST_BASE_BRANCH".."$BUILDKITE_COMMIT" | grep -qvE '(.md)'
 then
     echo "Only doc files were updated, not running the CI."
     exit
@@ -19,7 +24,6 @@ echo "Running pr-validation pipeline."
 echo "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"
 echo "$BUILDKITE_PULL_REQUEST_DRAFT"
 
-echo "steps:"
 
 if [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "develop" ] || [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ] || [[ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" =~ /^release\// ]] || [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "marvel/develop" ] || [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "test_ci" ] &&  ! BUILDKITE_PULL_REQUEST_DRAFT;
 then
