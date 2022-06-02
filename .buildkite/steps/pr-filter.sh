3711c5b833b06ac4543c14c08c931e7e3077e5f1
Joeffrey <joeffrey.chaucer.paitan@gmail.com>
2022-06-03 00:36:58 +0800

    Fix pipeline

--- .buildkite/steps/pr-filter.sh
+++ .buildkite/steps/pr-filter.sh
@@ -5,6 +5,7 @@ echo "$BUILDKITE_COMMIT"
 echo "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"
 
 git diff --stat "$BUILDKITE_PULL_REQUEST_BASE_BRANCH..$BUILDKITE_COMMIT"
+
 if ! git diff --name-only "$BUILDKITE_PULL_REQUEST_BASE_BRANCH..$BUILDKITE_COMMIT" | grep -qvE '(.md)'
 then
     echo "Only doc files were updated, not running the CI."
@@ -14,15 +15,10 @@ fi
 
 echo "Running pr-validation pipeline."
 
-    buildkite-agent pipeline upload <<YAML
-steps:
-  - label: "The pr pipeline"
-    command: "buildkite-agent pipeline upload .buildkite/pipeline.yml"
-    if: |
-      (build.pull_request.base_branch == "develop" ||
-      build.pull_request.base_branch == "main" ||
-      build.pull_request.base_branch =~ /^release\// ||
-      build.pull_request.base_branch == "marvel/develop" ||
-      build.pull_request.base_branch == "test_ci") &&
-      !build.pull_request.draft
-YAML
+echo "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"
+echo "$BUILDKITE_PULL_REQUEST_DRAFT"
+
+if [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "develop" ] || [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ] || [[ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" =~ /^release\// ]] || [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "marvel/develop" ] || [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "test_ci" ] &&  ! BUILDKITE_PULL_REQUEST_DRAFT;
+then
+    echo " - command: buildkite-agent pipeline upload .buildkite/pipeline.yml"
+fi
